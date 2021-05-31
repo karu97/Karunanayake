88b02d3a08452f6848d02d221cfc34b6544c30f4
karu97 <karunanayake234@gmail.com>
2021-05-31 23:36:18 +0530

    05 commit

---
+++ 30.05.2021/detail01.java
@@ -0,0 +1,8 @@
+class praveendetails{
+public static void main(String[] args){
+System.out.println("Name - Praveen Karunanayake");
+System.out.println("NIC - 199724702867");
+System.out.println("Contact Number - +94771703811");
+}
+}
+
