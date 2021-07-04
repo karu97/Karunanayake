06a24ac3637ac1f8597f498d92f005a470f25f5e
Praveen Karunanayake <85065184+karu97@users.noreply.github.com>
2021-07-04 23:27:02 +0530

    Ranger of byte

---
+++ Rangeofbyte.java
@@ -0,0 +1,10 @@
+public class Rangeofbyte {
+
+    public static void main(String[] args) {
+
+        byte x;
+        x=0;
+        System.out.println(x);
+
+	    }
+}
---
+++ Rangeofbyte01.java
@@ -0,0 +1,10 @@
+public class Rangeofbyte01 {
+
+    public static void main(String[] args) {
+
+        byte x;
+        x = 127;
+        System.out.println(x);
+
+	    }
+}
