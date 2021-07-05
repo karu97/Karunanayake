f9f1619b071dd7d610b55645a32708b7da8d8ee9
Praveen Karunanayake <85065184+karu97@users.noreply.github.com>
2021-07-05 21:53:54 +0530

    Details Forloop

---
+++ details_forloop.java
@@ -0,0 +1,9 @@
+class details_forloop {
+
+    public static void main(String[] args) {
+        String[] details = {"Praveen","Karunanayake","karunanayake234@gmail.com","0771703811"};
+    for(int i=0;i<4;i++){
+        System.out.println(details[i]);
+    }
+	    }
+}
