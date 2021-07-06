45f71ee7c98aff63ff3237cbe28e9a5ec342f338
Praveen Karunanayake <85065184+karu97@users.noreply.github.com>
2021-07-06 23:24:51 +0530

    While 20 - 10

---
+++ while20to10.java
@@ -0,0 +1,12 @@
+public class while20to10 {
+    public static void main(String[] args) {
+        int x = 20;
+        while(x>=10){
+            System.out.println(x);
+            x--;
+        }
+    }
+
+
+}
+
