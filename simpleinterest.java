a02cafe10e270fff0af32555e74b5c9afe67c552
Praveen Karunanayake <85065184+karu97@users.noreply.github.com>
2021-07-06 23:25:36 +0530

    Simple Intrerest

---
+++ simpleinterest.java
@@ -0,0 +1,8 @@
+class simpleinterest{
+public static void main(String args[]){
+// P = Principal amount, R = rate per annum, T = time in years.
+int p=3000,r=6,t=3,sum;
+sum=(p*r*t)/100;
+System.out.println(sum);
+}
+}
