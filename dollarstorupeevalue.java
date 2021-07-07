7112af971b58b1a6729a301faa72f59bf9baf0f0
Praveen Karunanayake <85065184+karu97@users.noreply.github.com>
2021-07-07 19:12:17 +0530

    dollars to rupeevalue

---
+++ dollarstorupeevalue.java
@@ -0,0 +1,9 @@
+class dollarstorupeevalue{
+public static void main(String args[]){
+	/* 1$= Rs.200.00  */
+	/* x=qty, y=The value of a dollar */
+int x=150,y=200,sum;
+sum=(x*y);
+System.out.println(sum);
+}
+}
