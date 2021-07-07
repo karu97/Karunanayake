0fbad652a1c799e3a8e40b741824682a22837993
Praveen Karunanayake <85065184+karu97@users.noreply.github.com>
2021-07-07 19:14:03 +0530

    division

---
+++ division.java
@@ -0,0 +1,7 @@
+public class division{
+public static void main(String[] args){
+int num1=42,num2=7;
+int result=num1/num2;
+System.out.println("division of" +num1+ "and" +num2+ "is" +result);
+}
+}
