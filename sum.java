5179da2556b210d07bc702e31237e5076f92c078
Praveen Karunanayake <85065184+karu97@users.noreply.github.com>
2021-07-09 19:18:53 +0530

    Calculate sum

---
+++ sum.java
@@ -0,0 +1,11 @@
+public class sum {
+    public static void main(String[] args) {
+        int num = 10, count = 1, total = 0;
+        while(count <= num)
+        {
+            total = total + count;
+            count++;
+        }
+        System.out.println("Sum of One to Ten numbers is: "+total);
+    }
+}
