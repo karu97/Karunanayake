8ab02d2ad912b2f14711b613648f7f37364af4d3
Praveen Karunanayake <85065184+karu97@users.noreply.github.com>
2021-07-10 18:13:54 +0530

    Factorial Numbers

---
+++ Factorialnumbers.java
@@ -0,0 +1,12 @@
+public class Factorialnumbers {
+    public static void main(String[] args) {
+
+        int fac = 1;
+        for (int y = 1; y <= 10; y++) {
+            fac = fac * y;
+            System.out.println(fac);
+        }
+		
+				System.out.println("is the factorial number 10" );
+    }
+}
