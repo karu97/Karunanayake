7c69d0a78b39ce20da7f6c6e78ecae2efe8c7573
Praveen Karunanayake <85065184+karu97@users.noreply.github.com>
2021-07-10 18:13:06 +0530

    While 8 - 20

---
+++ while8to20.java
@@ -0,0 +1,11 @@
+public class while8to20 {
+    public static void main(String[] args) {
+        int x=8;
+        while (x<=20){
+            System.out.println(x);
+            x++;
+        }
+    }
+
+
+}
