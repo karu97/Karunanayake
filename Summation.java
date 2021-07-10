f2f49fe0cf1ef67459305b389a77fda56c21c7eb
Praveen Karunanayake <85065184+karu97@users.noreply.github.com>
2021-07-10 18:12:08 +0530

    Summation

---
+++ Summation.java
@@ -0,0 +1,20 @@
+class Summation{
+
+   public static void main(String args[]) {
+
+    int sum=0;
+
+    for(int x=2;x<=100;x=x+2)
+
+    {
+
+          sum=sum+x;   
+
+          }
+
+        System.out.println("The sum of all even numbers upto 100 is"+sum);
+
+   }
+
+}
+
