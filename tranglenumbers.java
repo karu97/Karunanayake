afbe9f8649750423e15bc3c746bdcf19c5221db4
Praveen Karunanayake <85065184+karu97@users.noreply.github.com>
2021-07-11 22:01:02 +0530

    tranglenumbers

---
+++ tranglenumbers.java
@@ -0,0 +1,15 @@
+public class tranglenumbers {
+    public static void main(String[] args) {
+        int sum=0;
+        for (int i=1;i<=10;i++) {
+        sum=sum+i; 
+		}
+		
+		System.out.println("Trangle number of ten is " + sum);
+
+		
+	}
+}
+		
+			
+			
