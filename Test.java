442bf8ee63d62ba94fe0fa2329a5e2ad8e57c9fc
Praveen Karunanayake <85065184+karu97@users.noreply.github.com>
2021-07-19 13:52:27 +0530

    Test

---
+++ Test.java
@@ -0,0 +1,13 @@
+class Test{
+	public void method1(){
+		System.out.println("Method one is the second method other than main method");
+	}
+	
+	public static void main(String args []) {
+		Test x = new Test();
+		x.method1();
+	}
+}
+	
+	
+		
