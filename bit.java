7df76c16b599c3ea719ecf80c252dfdc780fa523
Praveen Karunanayake <85065184+karu97@users.noreply.github.com>
2021-07-19 14:36:19 +0530

    BIT

---
+++ bit.java
@@ -0,0 +1,20 @@
+class bit{
+    public void method01(){
+        System.out.println("I am Praveen Karunanayake");
+    }
+    public void method02(){
+        System.out.println("I study BIT Program");
+    }
+    public static void main (String args []){
+        bit x = new bit();
+        x.method01();
+        x.method02();
+        x.method03();
+    }
+    public void method03(){
+        System.out.println("I am in the first year");
+    }
+
+}		
+			
+	
