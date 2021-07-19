4976afaa55987fde91f4f8948450d7ff69fe4a96
Praveen Karunanayake <85065184+karu97@users.noreply.github.com>
2021-07-19 17:35:30 +0530

    Marks

---
+++ marks.java
@@ -0,0 +1,35 @@
+class marks{
+	public void science(){
+		System.out.println("Science Mark = A");
+	}
+	public void maths(){
+		System.out.println("Maths Mark = A");
+	}
+	public void english(){
+		System.out.println("English Mark = A");
+	}
+	public void sinhala(){
+		System.out.println("Sinhala Mark = A");
+	}
+	public void tamil(){
+		System.out.println("Tamil Mark = A");
+	}
+	public void history(){
+		System.out.println("History Mark = A");
+	}
+	public void it(){
+		System.out.println("IT Mark = A");
+	}
+	
+	public static void main (String args []){
+		marks x = new marks();
+			x.science();
+			x.maths();
+			x.english();
+			x.sinhala();
+			x.tamil();
+			x.history();
+			x.it();
+		}
+}		
+	
