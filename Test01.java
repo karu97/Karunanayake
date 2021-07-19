0d6e487087b08c9b66040fb4e300e453f792bcf0
Praveen Karunanayake <85065184+karu97@users.noreply.github.com>
2021-07-19 14:23:09 +0530

    Test 01

---
+++ Test01.java
@@ -0,0 +1,25 @@
+class Test01{
+	public void method01(){
+		System.out.println("The Programme");
+	}
+	public void method02(){
+		System.out.println("Is still loading");
+	}
+	
+	public static void main(String args []){
+		Test01 x = new Test01();
+			x.method01();
+			x.method02();
+			x.method03();
+	}
+	
+	public void method03(){
+		System.out.println("Wait");
+	}
+
+	
+}
+
+	
+		
+		
