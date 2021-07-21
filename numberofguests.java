1df3fea83188516287c73fb93b7ec13c0b6f130a
Praveen Karunanayake <85065184+karu97@users.noreply.github.com>
2021-07-21 13:17:46 +0530

    Numer of guests

---
+++ numberofguests.java
@@ -0,0 +1,13 @@
+class numberofguests{
+	public void method01(int a){
+		System.out.println("Number of guests in hall: " + a);
+	}
+
+	public static void main (String args []){
+		
+		numberofguests x = new numberofguests();
+		x.method01(50);
+	
+	}
+}	
+	
