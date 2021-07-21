7893c25f04d8b70e1ca64c65c1ef0d1a8ee6d4c1
Praveen Karunanayake <85065184+karu97@users.noreply.github.com>
2021-07-21 13:25:03 +0530

    Numer of students

---
+++ numberofstudents.java
@@ -0,0 +1,10 @@
+class numberofstudents{
+	public void method01(int a){
+		System.out.println("Number of students in the class :" + a);
+	}
+	
+	public static void main(String args []){
+		numberofstudents x = new numberofstudents();
+				x.method01(150);
+	}
+}	
