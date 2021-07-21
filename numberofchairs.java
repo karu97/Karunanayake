f8068a9ead50e53e6179661613915f903d5fc1b8
Praveen Karunanayake <85065184+karu97@users.noreply.github.com>
2021-07-21 15:25:45 +0530

    Number of Chairs

---
+++ numberofchairs.java
@@ -0,0 +1,15 @@
+class numberofchairs{
+	public void mehod01(int a){
+		System.out.println("Number of chairs at the class : " + a);
+	}
+
+	public void method02(int b){
+		System.out.println("Number of students in the class :" +b);
+	}
+	
+	public static void main (String args []){
+		numberofchairs x = new numberofchairs();
+			x.mehod01(150);
+			x.method02(100);
+	}
+}	
