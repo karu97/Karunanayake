cd1438895a8e95fa13f5e8e943e256a32a89431a
Praveen Karunanayake <85065184+karu97@users.noreply.github.com>
2021-07-21 15:31:11 +0530

    My age

---
+++ myage.java
@@ -0,0 +1,14 @@
+class myage {
+	public void method (int a){
+		System.out.println("My age :" + a);
+	}
+	
+	public static void main (String args []){
+		myage x = new myage();
+			x.method(23);
+			
+	}
+}
+	
+		
+
