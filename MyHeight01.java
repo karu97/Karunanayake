4791de2e8eda46f293e7f5a1fcb71ed7c2100dfa
Praveen Karunanayake <85065184+karu97@users.noreply.github.com>
2021-07-21 19:33:48 +0530

    MyHeight01

---
+++ MyHeight01.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class MyHeight01{
+		public static void main(String args []){
+		Scanner x = new Scanner(System.in);
+		System.out.println("Enter your height");
+		int personheight = x.nextInt();
+		System.out.println("My Height is: "  + personheight);
+	}
+}
+	
+		
