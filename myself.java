04248a6d77be20832d00d40b820fd76cfc1c5a73
Praveen Karunanayake <85065184+karu97@users.noreply.github.com>
2021-08-07 13:05:02 +0530

    My Self

---
+++ myself.java
@@ -0,0 +1,29 @@
+class myself{
+	public void method01() {
+	System.out.println("I am Praveen");
+	}
+	public void method02() {
+	System.out.println ("I am live in Gampaha");
+	}
+	public void method03() {
+	System.out.println ("I have one brother");
+	}
+	public void method04() {
+	System.out.println ("I got a pet parrot");
+	}
+	public void method05() {
+	System.out.println ("I go to univercity");
+	}
+
+public static void main (String [] args){
+	myself x = new myself();
+	
+		x.method01();
+		x.method02();
+		x.method03();
+		x.method04();
+		x.method05();
+	}
+}
+		
+	
