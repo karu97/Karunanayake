7272a6d13d9c9e944c89ce3c86bccbcc359a17a3
Praveen Karunanayake <85065184+karu97@users.noreply.github.com>
2022-01-06 20:33:00 +0530

    Exceptions

---
+++ Exceptions.java
@@ -0,0 +1,20 @@
+public class Exceptions{                                        
+    public static void main(String args[]){                     
+        int x=10,y=0,k=0;
+		int a[]=new int[4];		
+        try{                                                    
+            int z=x/y;                                         
+            for(int i=0; i<=4; i++)                             
+            {                                                                                           
+                a[i]=i+1;                                       
+            }                                                   
+        }                                                       
+        catch(Exception e){                                     
+                                                                
+            System.out.println("Division by zero is wrong");    
+        }
+		
+    }                                                           
+}                                                               
+                                                                
+                                                                
