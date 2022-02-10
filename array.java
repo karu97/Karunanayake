b1a27db42dc9e79cd0b078eaf1a4bc1013d81eb7
Praveen Karunanayake <85065184+karu97@users.noreply.github.com>
2022-02-10 13:43:34 +0530

    Array

---
+++ array.java
@@ -0,0 +1,13 @@
+public class array{
+	
+	public static void main (String args[]) {
+		float ar [ ] = {1, 2, 3, 4, 3.4f, 4.4f}; 
+		for(int i = 0; i < 6; i++) {
+		
+		System.out.println (ar [i]+" ");
+		
+		
+		}
+	}
+}	
+		
