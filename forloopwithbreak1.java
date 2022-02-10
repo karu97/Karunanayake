25bc61e3b47d13781431481c759ec3b3da309196
Praveen Karunanayake <85065184+karu97@users.noreply.github.com>
2022-02-10 13:33:51 +0530

    forloop with break

---
+++ forloopwithbreak1.java
@@ -0,0 +1,13 @@
+public class forloopwithbreak1 {
+	
+	public static void main (String args[]) {
+		for(int i = 10; i <= 100; i++) {
+		
+		System.out.println (i+=3);
+		System.out.println (i);
+		
+		
+		}
+	}
+}	
+		
